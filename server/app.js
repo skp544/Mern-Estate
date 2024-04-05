10472baab9087809c87e2f22a4d19394b326af93
skp544 <shubhamkumarprajapati544@gmail.com>
2024-04-05 22:42:59 +0530

    inital commit

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
